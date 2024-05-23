eedc96cbb2c66576b0157c53020a15bc3031b6e6
Kaushik <roykaushik354@gmail.com>
2024-05-23 14:50:13 +0530

    ADDED ALMOST

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import streamlit as st
+import  pandas
 
-st.set_page_config(layout="wide")
 
 col1,col2=st.columns(2)
 
 with col1:
-    st.image("images/Kaushik.png",width=400)
+    st.image("images/Kaushik.png",width=300)
 
 with col2:
     st.title("Kaushik Roy")
@@ -18,3 +18,21 @@ with col2:
 content2=""" Below are some basic python apps that I created while learning"""
 
 st.write(content2)
+
+col3,empty_col,col4=st.columns([1.5,0.5,1.5])
+df=pandas.read_csv("data.csv",sep=";")
+
+with col3:
+    for index,row in df[:10].iterrows():
+        st.header(row["title"])
+        st.write(row["description"])
+        st.image("images/"+row["image"])
+        st.write(f"[Source CODE] ({row['url']})")
+
+
+with col4:
+    for index,row in df[10:].iterrows():
+        st.header(row["title"])
+        st.write(row["description"])
+        st.image("images/"+row["image"])
+        st.write(f"[Source CODE] ({row['url']})")
